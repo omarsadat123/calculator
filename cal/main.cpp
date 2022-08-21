07a390067b85e48306ae14807cfd1cf1bfd05d9e
omarsadat123 <111651638+omarsadat123@users.noreply.github.com>
2022-08-21 11:55:49 +0600

    file updated

--- cal/main.cpp
+++ cal/main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    system("color 72");
+    system("color 74");
     float x,y,result;
     char op;
     cout<< "            \n\t\t      Welcome to you\n\n\t\t   Hope that you are well.\n\n";
